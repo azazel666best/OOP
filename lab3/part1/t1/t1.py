a7ff9886be93defea92fad147995ef58db0bab72
azazel666best <serjio2003@ukr.net>
2021-11-24 19:44:54 +0200

    lab 2

--- lab3/part1/t1/t1.py
+++ lab3/part1/t1/t1.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import json
 
 
 class Event:
@@ -56,15 +57,15 @@ class Event:
         self.__date = date
 
     def __str__(self):
-        return f'{self.__name}, {self.__num_of_tickets}, {self.__base_price}, {self.__date}'
+        return f'Name: {self.__name}\nNum of tickets: {self.__num_of_tickets}\nBase price: {self.__base_price}\n' \
+               f'Date: {self.__date}'
 
 
 class Ticket:
-    def __init__(self, event, ticket_id, date, is_student=False):
+    def __init__(self, event, ticket_id, price):
         self.event = event
         self.ticket_id = ticket_id
-        self.is_student = is_student
-        self.date = date
+        self.price = price
 
     @property
     def event(self):
@@ -75,18 +76,20 @@ class Ticket:
         return self.__ticket_id
 
     @property
-    def is_student(self):
-        return self.__is_student
-
-    @property
-    def date(self):
-        return self.__date
+    def price(self):
+        return self.__price
 
     @event.setter
     def event(self, event):
         if not isinstance(event, Event):
             raise TypeError('event mast be Event')
         self.__event = event
+
+    @price.setter
+    def price(self, price):
+        if not isinstance(price, (int, float)):
+            raise TypeError('price mast be int or float')
+        self.__price = price
 
     @ticket_id.setter
     def ticket_id(self, ticket_id):
@@ -100,38 +103,57 @@ class Ticket:
         self.__event.num_of_tickets -= 1
         self.__event.tickets_id.append(ticket_id)
 
-    @is_student.setter
-    def is_student(self, is_student):
-        if not isinstance(is_student, bool):
-            raise TypeError('is student mast be bool')
-        self.__is_student = is_student
-
-    @date.setter
-    def date(self, date):
-        if not isinstance(date, datetime):
-            raise TypeError('date mast be datatime')
-        self.__date = date
-
-    def get_price(self):
-        if self.is_student:
-            return round(self.event.base_price * 0.5, 2)
-        elif (self.event.date - self.date).days >= 60:
-            return round(self.event.base_price * 0.6, 2)
-        elif (self.event.date - self.date).days < 10:
-            return round(self.event.base_price * 1.1, 2)
-        else:
-            return round(self.event.base_price, 2)
-
     def __str__(self):
-        return f'{self.ticket_id, self.is_student}'
+        return f'{self.event}\nTicket Id: {self.ticket_id}\nTicket price: {self.price}'
+
+
+class UsualTicket(Ticket):
+    def __init__(self, event, ticket_id):
+        super(UsualTicket, self).__init__(event, ticket_id, round(event.base_price, 2))
+
+
+class AdvanceTicket(Ticket):
+    def __init__(self, event, ticket_id):
+        with open('type_of_tickets') as file:
+            super(AdvanceTicket, self).__init__(event, ticket_id, round(event.base_price *
+                                                                        json.load(file)['AdvanceTicket'], 2))
+
+
+class StudentTicket(Ticket):
+    def __init__(self, event, ticket_id):
+        with open('type_of_tickets') as file:
+            super(StudentTicket, self).__init__(event, ticket_id, round(event.base_price *
+                                                                        json.load(file)['StudentTicket'], 2))
+
+
+class LateTicket(Ticket):
+    def __init__(self, event, ticket_id):
+        with open('type_of_tickets') as file:
+            super(LateTicket, self).__init__(event, ticket_id, round(event.base_price *
+                                                                        json.load(file)['LateTicket'], 2))
+
+
+def get_ticket(event, ticket_id, date, is_student=False):
+    if is_student:
+        return StudentTicket(event, ticket_id)
+    elif (event.date - date).days >= 60:
+        return AdvanceTicket(event, ticket_id)
+    elif (event.date - date).days < 10:
+        return LateTicket(event, ticket_id)
+    else:
+        return UsualTicket(event, ticket_id)
 
 
 try:
-    obj = Event('asd', 2, 12.5, datetime(2025, 7, 14, 12, 30))
-    ticket_1 = Ticket(obj, 152551, datetime(2025, 7, 12, 12, 30), False)
-    ticket_2 = Ticket(obj, 152552, datetime(2025, 7, 12, 12, 30), False)
-    print(obj.base_price)
-    print(ticket_1.get_price())
+    obj = Event('asd', 4, 12, datetime(2025, 7, 14, 12, 30))
+    ticket_us = get_ticket(obj, 152551, datetime(2025, 6, 13, 12, 30))
+    ticket_st = get_ticket(obj, 152552, datetime(2025, 7, 13, 12, 30), True)
+    ticket_ad = get_ticket(obj, 152553, datetime(2025, 7, 13, 12, 30))
+    ticket_lt = get_ticket(obj, 152554, datetime(2025, 5, 13, 12, 30))
+    print(ticket_us, end='\n\n')
+    print(ticket_st, end='\n\n')
+    print(ticket_ad, end='\n\n')
+    print(ticket_lt, end='\n\n')
 except TypeError as er:
     print('TypeError', er)
 except ValueError as er:
--- lab3/part1/t2/t2.py
+++ lab3/part1/t2/t2.py
@@ -1,9 +1,8 @@
 from re import match
 import json
-# import datetime
 
 
-class Pizza:
+class Dish:
     def __init__(self, name, price, description):
         self.name = name
         self.price = price
@@ -24,21 +23,21 @@ class Pizza:
     @name.setter
     def name(self, name):
         if not isinstance(name, str):
-            raise TypeError
+            raise TypeError('name mast be str')
         self.__name = name
 
     @price.setter
     def price(self, price):
         if not isinstance(price, (int, float)):
-            raise TypeError
+            raise TypeError('price mast be int or float')
         if price <= 0:
-            raise ValueError
+            raise ValueError('price mast be >0')
         self.__price = price
 
     @description.setter
     def description(self, description):
         if not isinstance(description, str):
-            raise TypeError
+            raise TypeError('description mast be str')
         self.__description = description
 
     def __str__(self):
@@ -66,21 +65,21 @@ class Customer:
     @surname.setter
     def surname(self, surname):
         if not isinstance(surname, str):
-            raise TypeError
+            raise TypeError('surname mast be str')
         self.__surname = surname
 
     @name.setter
     def name(self, name):
         if not isinstance(name, str):
-            raise TypeError
+            raise TypeError('name mast be str')
         self.__name = name
 
     @mobile_phone.setter
     def mobile_phone(self, mobile_phone):
         if not isinstance(mobile_phone, str):
-            raise TypeError
+            raise TypeError('mobile_phone mast be str')
         if not match(r'\+380[\d]{9}$', mobile_phone):
-            raise ValueError
+            raise ValueError('mobile_phone mast be like +380762345657')
         self.__mobile_phone = mobile_phone
 
     def __str__(self):
@@ -91,7 +90,7 @@ class Order:
     def __init__(self, new_customer, product, **ingredients):
         self.customer = new_customer
         self.product = product
-        self.__ingredients = ingredients
+        self.ingredients = ingredients
 
     @property
     def customer(self):
@@ -108,22 +107,23 @@ class Order:
     @customer.setter
     def customer(self, new_customer):
         if not isinstance(new_customer, Customer):
-            raise TypeError
+            raise TypeError('customer mast be Customer')
         self.__customer = new_customer
 
     @product.setter
     def product(self, product):
-        if not isinstance(product, Pizza):
-            raise TypeError
+        if not isinstance(product, Dish):
+            raise TypeError('product mast be Pizza')
         self.__product = product
 
     @ingredients.setter
-    def ingredients(self, **ingredients):
+    def ingredients(self, ingredients):
         if not all(isinstance(x, str) for x in list(ingredients.keys())):
-            raise TypeError
+            raise TypeError('ingredients mast be Pizza')
         with open('ingredients', 'r') as file:
-            if not all(x in file for x in list(ingredients.keys())):
-                raise ValueError
+            ing_list = json.load(file)
+            if not all(x in ing_list for x in list(ingredients.keys())):
+                raise ValueError('no ingredient')
 
         self.__ingredients = ingredients
 
@@ -133,18 +133,38 @@ class Order:
                                     list(self.__ingredients.keys()))))
 
 
+class PizzaOfTheDay(Dish):
+    def __init__(self, name, price, description, day):
+        super(PizzaOfTheDay, self).__init__(name, price, description)
+        self.day = day
+
+    @property
+    def day(self):
+        return self.__day
+
+    @day.setter
+    def day(self, day):
+        if not isinstance(day, int):
+            raise TypeError('day mast be int')
+        if day < 0 or day > 6:
+            raise TypeError('day mast be in range(7)')
+        self.__day = day
+
+
 class CustomEncoder(json.JSONEncoder):
 
     def default(self, o):
-        if not isinstance(o, Pizza):
+        if not isinstance(o, PizzaOfTheDay):
             raise TypeError
         return {'__{}__'.format(o.__class__.__name__): o.__dict__}
 
 
 def decode_object(o):
-    if '__Pizza__' in o:
-        return Pizza(o['__Pizza__']['_Pizza__name'], o['__Pizza__']['_Pizza__price'],
-                     o['__Pizza__']['_Pizza__description'])
+    if '__PizzaOfTheDay__' in o:
+        return PizzaOfTheDay(o['__PizzaOfTheDay__']['_Dish__name'],
+                             o['__PizzaOfTheDay__']['_Dish__price'],
+                             o['__PizzaOfTheDay__']['_Dish__description'],
+                             o['__PizzaOfTheDay__']['_PizzaOfTheDay__day'])
     return o
 
 
@@ -154,7 +174,7 @@ try:
 
     customer = Customer('a', 's', '+380999999999')
 
-    order = Order(customer, pizza_of_the_day, asd=1, bcd=2)
+    order = Order(customer, pizza_of_the_day, a=1)
 
     print(order)
 except TypeError as er:
